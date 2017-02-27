94939e91e24bf4dca462b61038432a6d7fd78649
Partha Gundarpi <buzzpartha@gmail.com>
2017-02-27 09:44:04 +0530

    Update InputIntArray.java

--- InputIntArray.java
+++ InputIntArray.java
@@ -14,3 +14,17 @@ class InputIntArray{
        //System.out.println(values.get(0));
     }
 }
+
+class Main{
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int M = 3; // 3 lines;
+        int N =2; // each line having 2 integers
+        Integer[][] values = new Integer[M][N];
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<N; j++) {
+                values[i][j] = sc.nextInt();
+            }
+        }
+    }
+}
